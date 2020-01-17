73873e0226ea9a9199278cdc7d2ef84922846128
mygit999 <54948812+mygit999@users.noreply.github.com>
2020-01-17 16:24:18 +0530

    Update PipelineScript.groovy

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -21,7 +21,7 @@ pipeline {
          }
         stage('ATC Checks'){
               parallel{
-                    stage('ATC Checks HANA Readiness - FUNCTIONAL_DB'){
+                    stage('ATC Checks - DEFAULT'){
                         steps{
                             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                             sh ''' 
@@ -30,15 +30,7 @@ pipeline {
                            }
                         }
                      }
-                     stage('ATC Checks HANA Readiness - FUNCTIONAL_DB_ADDITION'){
-                           steps{
-                           withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                            sh ''' 
-                           newman run hhttps://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc_funcdbadd.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops"
-                           '''
-                           }
-                        }
-                     }
+                    
                }
          }      
    }
