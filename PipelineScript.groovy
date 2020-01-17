43b33ebdcdc7e6b5413da49e12520947812328e9
himanshush13 <36809136+himanshush13@users.noreply.github.com>
2020-01-17 13:31:15 +0530

    Update PipelineScript.groovy

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -5,7 +5,7 @@ pipeline {
                steps{
                      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                      sh '''
-                     newman run https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/01_ABAP_Unit/abap_unit.postman_collection.json -k --bail --environment https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/01_ABAP_Unit/abap_unit.postman_environment.json -k --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=NAME OF YOUR PROJECT/PACKAGE" 
+                     newman run https://github.com/himanshush13/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_collection.json -k --bail --environment https://github.com/himanshush13/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_environment.json -k --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
                      '''
                      }
                 }
@@ -14,7 +14,7 @@ pipeline {
                steps{
                     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                     sh ''' 
-                    newman run https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/02_ABAP_Coverage/abap_coverage_analysis.postman_collection.json -k --bail --environment https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/02_ABAP_Coverage/abap_coverage.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=NAME OF YOUR PROJECT/PACKAGE" 
+                    newman run https://github.com/himanshush13/ABAPDEVOPS.git/master/02_ABAP_Coverage/abap_coverage_analysis.postman_collection.json -k --bail --environment https://github.com/himanshush13/ABAPDEVOPS.git/master/02_ABAP_Coverage/abap_coverage.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
                     '''
                     }
                }
@@ -25,7 +25,7 @@ pipeline {
                         steps{
                             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                             sh ''' 
-                           newman run https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/03_ATC_Checks/abap_atc_funcdb.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=NAME OF YOUR PROJECT/PACKAGE"
+                           newman run https://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc_funcdb.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops"
                            '''
                            }
                         }
@@ -34,7 +34,7 @@ pipeline {
                            steps{
                            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ID OF YOUR CREDENTIALS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                             sh ''' 
-                           newman run https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://raw.githubusercontent.com/lechnerc77/ABAP_CI_PIPELINE_BASE/master/03_ATC_Checks/abap_atc_funcdbadd.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=NAME OF YOUR PROJECT/PACKAGE"
+                           newman run hhttps://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/himanshush13/ABAPDEVOPS.git/master/03_ATC_Checks/abap_atc_funcdbadd.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops"
                            '''
                            }
                         }
